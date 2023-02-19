3f9f00cce72e594ff4bfddf8ca493f3b48410d92
Abrar Khan <30430433+abrar-khan007@users.noreply.github.com>
2023-02-19 22:42:21 +0530

    Revert "reverso"

--- angular-app/src/app/core/components/nav.component.ts
+++ angular-app/src/app/core/components/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -14,39 +14,6 @@ import { Component, OnInit } from '@angular/core';
         </a>
       </ul>
     </nav>
-    <nav class="menu auth">
-      <p class="menu-label">Auth</p>
-      <ul class="menu-list">
-        <div class="auth">
-          <a *ngIf="!userInfo" href="/login/t">Twitter</a>
-          <a *ngIf="!userInfo" href="/login/g">GitHub</a>
-          <a *ngIf="!userInfo" href="/.auth/login/facebook">FaceBook</a>
-          <a *ngIf="userInfo" href="/logout">Logout</a>
-          <div class="user" *ngIf="userInfo">
-            <p>{{ userInfo?.userDetails }}</p>
-            <p>({{ userInfo?.identityProvider }})</p>
-          </div>
-        </div>
-      </ul>
-    </nav>
   `,
 })
-export class NavComponent implements OnInit {
-  userInfo: {
-    identityProvider: string;
-    userId: string;
-    userDetails: string;
-    userRoles: string[];
-  } = null;
-
-  async ngOnInit(): Promise<void> {
-    this.userInfo = await this.getUserInfo();
-  }
-
-  async getUserInfo() {
-    const response = await fetch('/.auth/me');
-    const payload = await response.json();
-    const { clientPrincipal } = payload;
-    return clientPrincipal;
-  }
-}
+export class NavComponent {}
